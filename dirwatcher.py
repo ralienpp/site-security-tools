72a8c66906f93f99eebcaed8a167563262d052f5
U-SLIPPERY\alex <C:\Soft\TheBat\Mail>
2016-09-02 00:01:03 +0200

    made pylint a bit happier

--- dirwatcher.py
+++ dirwatcher.py
@@ -8,9 +8,10 @@ import shutil
 import argparse
 import sys
 
-BLOCK = 2**16 #for reading file chunks
+BLOCK = 2**16 # for reading file chunks
+KEYNOTFOUND = '<KEYNOTFOUND>'
 
-def HashFile(path):
+def hash_file(path):
     '''Compute the hash of a file specified by the full path to it'''
     f = open(path, 'rb')
     digest = hashlib.md5()
@@ -23,40 +24,39 @@ def HashFile(path):
 
 
 
-def BuildTree(path, exceptions=None, maxsize=None):
-    tree = {}
+def build_tree(path, exceptions=None, maxsize=None):
+    result = {}
     for dirName, subDirs, files in os.walk(path):
-        #add found files to index
+        # add found files to index
         for item in files:
             fullPath = os.path.join(dirName, item)
             size = os.path.getsize(fullPath)
-            # import pdb; pdb.set_trace()
             if maxsize and size > maxsize:
                 # print 'skipping ', fullPath
                 continue
-            fingerprint = HashFile(fullPath)
-            tree[fullPath] = fingerprint
+            fingerprint = hash_file(fullPath)
+            result[fullPath] = fingerprint
 
-        #remove ignored directories, if necessary
+        # remove ignored directories, if necessary
         if exceptions:
             for item in exceptions:
                 if item in subDirs:
                     subDirs.remove(item)
 
-    return tree
+    return result
 
 
-def PrintTree(tree):
-    for key, value in tree.iteritems():
+def print_tree(data):
+    for key, value in data.iteritems():
         print '%s\t%s' % (value, key)
 
 
-def DumpTree(obj, path):
+def dump_tree(obj, path):
     '''Store the directory tree and hashes in a pickle at the given path'''
     with open(path, 'wb') as f:
         pickle.dump(obj, f)
 
-def GetTree(path):
+def get_tree(path):
     '''Load a directory tree and accompanying hashes from a pickle
     produced at the previous run'''
     with open(path, 'rb') as f:
@@ -71,7 +71,6 @@ def dict_diff(first, second):
         @param second:  Second dicationary to diff.
         @return diff:   Dict of Key => (first.val, second.val)
     """
-    KEYNOTFOUND = '<KEYNOTFOUND>'
     diff = {}
     # Check all keys in first dict
     for key in first.keys():
@@ -107,30 +106,30 @@ if __name__ == '__main__':
     os.chdir(args.store)
 
     try:
-        original = GetTree('canonical.tree')
+        original = get_tree('canonical.tree')
     except IOError, err:
         print err
         print 'no original tree exists, building it'
         print 'this will take a while'
-        tree = BuildTree(PATH, IGNORE, args.maxsize)
-        DumpTree(tree, 'canonical.tree') #will be subsequently updated
-        DumpTree(tree, 'canonical.tree-start-point') #always constant
+        tree = build_tree(PATH, IGNORE, args.maxsize)
+        dump_tree(tree, 'canonical.tree') #will be subsequently updated
+        dump_tree(tree, 'canonical.tree-start-point') #always constant
         print 'Done, quitting.'
         sys.exit(0)
     else:
         # we've found an earlier tree, let's build a new one and compare
         # we're not printing anything to stdout, not to cause unnecessary emails
         # sent by cron; we print only if the admin's attention is required
-        currentTree = BuildTree(PATH, IGNORE, args.maxsize)
+        currentTree = build_tree(PATH, IGNORE, args.maxsize)
         delta = dict_diff(original, currentTree)
         if delta:
-            DumpTree(currentTree, '%s.tree' % timestamp)
+            dump_tree(currentTree, '%s.tree' % timestamp)
             print 'Differences found: ', len(delta)
-            PrintTree(delta)
+            print_tree(delta)
 
-            #the output of the cronjob will be sent by email
-            #to avoid duplicate notifications, make the current
-            #tree the original
+            # the output of the cronjob will be sent by email
+            # to avoid duplicate notifications, make the current
+            # tree the original
             os.unlink('canonical.tree')
             shutil.copy('%s.tree' % timestamp, 'canonical.tree')
             os.rename('%s.tree' % timestamp, 'canonical.tree')
